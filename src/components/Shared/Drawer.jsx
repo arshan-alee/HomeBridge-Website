2ca08251b397be6db904c34c3854bf746f7b086a
ShayanJamil519 <shayanjamil500@gmail.com>
2024-01-13 18:19:10 +0500

    UI Fixes

---
+++ src/components/Shared/Drawer.jsx
@@ -0,0 +1,48 @@
+import { RxCross1 } from "react-icons/rx";
+import { Link } from "react-router-dom";
+
+const Drawer = ({ isOpen, onClose }) => {
+  return (
+    <div
+      className={`fixed z-50 top-0 right-0 h-full w-[80%] bg-[#00CE3A] text-white px-6 pt-7 ${
+        isOpen ? "transform translate-x-0" : "transform translate-x-full"
+      } transition-transform ease-in-out duration-300`}
+    >
+      <div className="flex justify-between items-center  ">
+        <Link to="/login" className="text-[20px]">
+          Login/Register
+        </Link>
+        <div
+          className="text-white bg-[#00a52e] w-[40px] h-[40px] grid place-items-center rounded-md"
+          onClick={onClose}
+        >
+          <RxCross1 className="text-[25px]" />
+        </div>
+      </div>
+
+      <div className="flex flex-col text-[20px]">
+        <Link className="mb-5 mt-2" to="/" onClick={onClose}>
+          Home
+        </Link>
+        <Link className="mb-5" to="/f_2_r" onClick={onClose}>
+          F-2-R
+        </Link>
+        <Link className="mb-5" to="/job_house" onClick={onClose}>
+          Job&House
+        </Link>
+        <Link to="/event" onClick={onClose}>
+          Event
+        </Link>
+      </div>
+
+      <div className="mt-12 flex flex-col gap-2">
+        <Link className="font-light text-[14px]">Privacy Policy</Link>
+        <Link className="font-light text-[14px]">Terms of Service</Link>
+      </div>
+
+      {/* Add other drawer content here */}
+    </div>
+  );
+};
+
+export default Drawer;
--- src/components/Shared/Header.jsx
+++ src/components/Shared/Header.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useRef, useState } from "react";
 import { RxCross1 } from "react-icons/rx";
 import { Link, useLocation } from "react-router-dom";
+import Drawer from "./Drawer";
+import ProfileDropDown from "./ProfileDropDown";
 
 const linksArray = [
   {
@@ -26,87 +28,6 @@ const linksArray = [
 ];
 
 // Profile DropDown
-const ProfileDropDown = ({
-  isMobile,
-  setOpenProfileDropdown,
-  openProfileDropDown,
-}) => {
-  return (
-    <div
-      className={`absolute ${
-        isMobile
-          ? " -left-7 top-10 lg:hidden block"
-          : "right-0 top-10 lg:block hidden"
-      } z-50 rounded-md w-[200px] sm:w-[220px] bg-[#fff]  `}
-    >
-      <div className="pt-4 pb-5 sm:pt-5 sm:pb-6 border-b-[0.5px] border-[#AAA]">
-        <img
-          src="/icons/user_circle.png"
-          alt="logo"
-          className="w-[40px] sm:w-[60px] mx-auto mb-1"
-        />
-        <p className="font-semibold text-center">Jason</p>
-      </div>
-      <div className="w-full flex ">
-        <div className="w-1/2 py-3 sm:py-4 text-center border-r-[0.5px] border-[#AAA]">
-          <Link
-            className="text-[#2B2B2B] text-[12px]"
-            to="/mypage"
-            onClick={() => setOpenProfileDropdown(!openProfileDropDown)}
-          >
-            Mypage{" "}
-          </Link>
-        </div>
-        <div className="w-1/2 py-3 sm:py-4 text-center">
-          <Link
-            className="text-[#2B2B2B] text-[12px]"
-            onClick={() => setOpenProfileDropdown(!openProfileDropDown)}
-          >
-            logout{" "}
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-// Mobile Devices
-const Drawer = ({ isOpen, onClose }) => {
-  return (
-    <div
-      className={`fixed z-50 top-0 right-0 h-full w-[80%] bg-[#00CE3A] text-white px-6 pt-7 ${
-        isOpen ? "transform translate-x-0" : "transform translate-x-full"
-      } transition-transform ease-in-out duration-300`}
-    >
-      <div className="flex justify-between items-center  ">
-        <Link to="/login">Login/Register</Link>
-        <div
-          className="text-white bg-[#00a52e] w-[40px] h-[40px] grid place-items-center rounded-md"
-          onClick={onClose}
-        >
-          <RxCross1 className="text-[25px]" />
-        </div>
-      </div>
-
-      <div className="flex flex-col">
-        <Link className="mb-5 mt-2" to="/" onClick={onClose}>
-          Home
-        </Link>
-        <Link className="mb-5" to="/f_2_r" onClick={onClose}>
-          F-2-R
-        </Link>
-        <Link className="mb-5" to="/job_house" onClick={onClose}>
-          Job&House
-        </Link>
-        <Link to="/event" onClick={onClose}>
-          Event
-        </Link>
-      </div>
-
-      {/* Add other drawer content here */}
-    </div>
-  );
-};
 
 const Header = () => {
   const location = useLocation();
