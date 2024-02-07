20cc27aeff597ca6da3ba71e6a71ebe66ca88886
ShayanJamil519 <shayanjamil500@gmail.com>
2024-02-07 10:02:30 +0500

    Fixes

--- src/components/MyPage/Event/EventInfo.jsx
+++ src/components/MyPage/Event/EventInfo.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { convertDate } from "../../../utils/helper";
 
 function EventInfo({ data }) {
   const { departure, arrival, traffic, price } = data;
@@ -7,15 +8,37 @@ function EventInfo({ data }) {
       <h1 className="text-[24px] font-bold bg-[#00CE3A] text-white pl-6 lg:pl-12 py-3">
         Event Information
       </h1>
-      <div className="flex flex-wrap p-6 lg:p-12 break-words ">
-        <div className="flex w-full lg:w-1/2 my-2">
-          <p className={`w-1/2 text-[#00CE3A] `}>Departure</p>
+      <div className="grid grid-cols-2  p-6 lg:p-12 break-words ">
+        <div className="col-span-1 flex justify-between my-2">
+          <p className={`w-1/2 text-[#00CE3A] font-semibold `}>Departure</p>
           <p
             className={`w-1/2 font-medium text-[16px] text-[#2B2B2B] mr-1 lg:mr-10`}
           >
-            {departure}
+            {convertDate(departure)}
+          </p>
+        </div>
+        <div className="col-span-1 flex justify-between my-2">
+          <p className={`w-1/2 text-[#00CE3A] font-semibold `}>traffic</p>
+          <p
+            className={`w-1/2 font-medium text-[16px] text-[#2B2B2B] mr-1 lg:mr-10`}
+          >
+            {traffic}
+          </p>
+        </div>
+        <div className="col-span-1 flex justify-between my-2">
+          <p className={`w-1/2 text-[#00CE3A] font-semibold `}>Arrival</p>
+          <p
+            className={`w-1/2 font-medium text-[16px] text-[#2B2B2B] mr-1 lg:mr-10`}
+          >
+            {convertDate(arrival)}
+          </p>
+        </div>
+        <div className="col-span-1 flex justify-between my-2">
+          <p className={`w-1/2 text-[#00CE3A] font-semibold `}>Price</p>
+          <p
+            className={`w-1/2 font-medium text-[16px] text-[#2B2B2B] mr-1 lg:mr-10`}
+          >
             {price}
-            {arrival}
           </p>
         </div>
       </div>
---
+++ src/utils/helper.js
@@ -0,0 +1,22 @@
+export const convertDate = (inputDate) => {
+  const date = new Date(inputDate);
+
+  // Array to hold day names
+  const days = ["일", "월", "화", "수", "목", "금", "토"];
+
+  // Extract year, month, day, and day of the week from the date object
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1; // Months are 0-indexed, so add 1
+  const day = date.getDate();
+  const dayOfWeek = days[date.getDay()]; // Get the day of the week
+
+  // Adjust the time to the desired timezone (in this case, 7 hours ahead)
+  const hours = date.getUTCHours() + 7;
+  const minutes = date.getMinutes();
+
+  const formattedDate = `${year}.${month}.${day}(${dayOfWeek}) ${
+    hours < 10 ? "0" + hours : hours
+  }:${minutes < 10 ? "0" + minutes : minutes}`;
+
+  return formattedDate;
+};
