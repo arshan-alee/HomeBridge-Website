426df1bac8886e1fd3eb79c7a2a5de3203a307e2
ShayanJamil519 <shayanjamil500@gmail.com>
2024-02-24 19:49:05 +0500

    Fixes

--- src/components/MyPage/JobPage/JobDetail.jsx
+++ src/components/MyPage/JobPage/JobDetail.jsx
@@ -1,25 +1,6 @@
 import React from "react";
 
 function JobDetail({ data }) {
-  const jobInfo = {
-    "Job main categories": "Lorem",
-    "Job subcategories": "Sagittis",
-    "Types of jobs": "We are",
-    "Work start date": "We are",
-    "Application Deadline": "We are",
-    "Visa type": "We are",
-    Language: "We are",
-    Gender: "We are",
-    Age: "20 ~ 32",
-  };
-
-  const jobBenefits = {
-    "Hourly wage": "10,000KRW",
-    "A working day": "Monday, Tuesday, Wednesday",
-    "Working hours": "9",
-    Bonus: "0",
-  };
-
   return (
     <div className="w-full lg:w-2/3 mx-4 md:mx-12">
       <div className="lg:py-4 border-b border-[#E2E2E2] py-8">
