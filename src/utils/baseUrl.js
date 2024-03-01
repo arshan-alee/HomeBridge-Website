4040c6d84d228842fe8ac56c60572dc87cd268eb
M-Abdullah-Bhatti <87266652+M-Abdullah-Bhatti@users.noreply.github.com>
2024-03-01 20:07:11 +0500

    base url changed

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -4,7 +4,7 @@ const environment = "production";
 let baseUrl;
 if (environment === "production") {
   // baseUrl = "https://black-panda-slip.cyclic.app";
-  baseUrl = "https://home-bridge-backend-0e155295c3cc.herokuapp.com";
+  baseUrl = "https://homebridge-backend-c67d8f57f4c3.herokuapp.com";
 } else {
   baseUrl = "http://localhost:5000";
   // baseUrl = "http://192.168.1.108:5000";
