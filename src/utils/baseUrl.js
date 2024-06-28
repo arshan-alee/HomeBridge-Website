c1732381f1fc17a628d6923817f7406cfe72538e
Mir Arshan Ali <109890622+arshan-alee@users.noreply.github.com>
2024-06-28 14:35:31 +0500

    Update baseUrl.js

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -4,7 +4,7 @@ const environment = "production";
 let baseUrl;
 if (environment === "production") {
   // baseUrl = "https://black-panda-slip.cyclic.app";
-  baseUrl = "https://black-panda-slip.cyclic.app";
+  baseUrl = "https://home-bridge-backend.vercel.app";
 } else {
   baseUrl = "http://localhost:5000";
   // baseUrl = "http://192.168.1.108:5000";
